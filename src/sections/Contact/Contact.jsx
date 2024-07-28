02d842e8255f93cbd75611aa432f40eb7c32ac97
YASH MAHINDROO <mahindrooy@gmail.com>
2024-07-28 22:15:17 +0530

    third commit

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -1,10 +1,11 @@
 import styles from './ContactStyles.module.css';
+import emailjs from '@emailjs/browser';
 
 function Contact() {
   return (
     <section id="contact" className={styles.container}>
       <h1 className="sectionTitle">Contact</h1>
-      <form action="">
+      <form action="https://formspree.io/f/xanweldw" method='POST'>
         <div className="formGroup">
           <label htmlFor="name" hidden>
             Name
